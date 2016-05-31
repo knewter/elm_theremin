54c4b68e1f6eb7a13b7733261d5d05b7e6eae3ef
Josh Adams <josh.rubyist@gmail.com>
2016-05-30 19:51:41 -0500

    Update to the proper mouse-y theremin

--- src/Main.elm
+++ src/Main.elm
@@ -1,26 +1,47 @@
-module Main exposing (..)
+port module Main exposing (..)
 
-import Html exposing (Html, div, button, text)
+import Html exposing (Html, div, button, text, h1, section)
 import Html.Events exposing (onClick)
 import Html.App as App
+import Window
+import Task exposing (andThen)
+import Mouse
 
 
 type alias Model =
-    Int
+    { gainValue : Float
+    , frequencyValue : Float
+    , windowWidth : Int
+    , windowHeight : Int
+    }
 
 
 type Msg
-    = Increment
-    | Decrement
+    = IncrementGain
+    | DecrementGain
+    | IncrementFrequency
+    | DecrementFrequency
+    | UpdateDimensions { width : Int, height : Int }
+    | UpdateMouse ( Int, Int )
+    | NoOp
 
 
 model =
-    0
+    { gainValue = 0.001
+    , frequencyValue = 3000
+    , windowWidth = 100
+    , windowHeight = 100
+    }
+
+
+getInitialWindowSize : Cmd Msg
+getInitialWindowSize =
+    Task.perform (\_ -> NoOp) UpdateDimensions Window.size
 
 
 main =
     App.program
-        { init = ( model, Cmd.none )
+        { init = ( model, getInitialWindowSize )
         , view = view
         , update = update
         , subscriptions = subscriptions
@@ -29,23 +50,90 @@ main =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Sub.batch
+        [ Window.resizes UpdateDimensions
+        , Mouse.moves (\{ x, y } -> UpdateMouse ( x, y ))
+        ]
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (toString model) ]
-        , button [ onClick Increment ] [ text "+" ]
+        [ section []
+            [ h1 [] [ text "Gain" ]
+            , button [ onClick DecrementGain ] [ text "-" ]
+            , div [] [ text (toString model.gainValue) ]
+            , button [ onClick IncrementGain ] [ text "+" ]
+            ]
+        , section []
+            [ h1 [] [ text "Frequency" ]
+            , button [ onClick DecrementFrequency ] [ text "-" ]
+            , div [] [ text (toString model.frequencyValue) ]
+            , button [ onClick IncrementFrequency ] [ text "+" ]
+            ]
+        , section []
+            [ h1 [] [ text "Dimensions" ]
+            , div [] [ text (toString model.windowHeight) ]
+            , div [] [ text (toString model.windowWidth) ]
+            ]
         ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            ( model + 1, Cmd.none )
+        IncrementGain ->
+            let
+                newModel =
+                    { model | gainValue = model.gainValue + 0.001 }
+            in
+                ( newModel, audio newModel )
 
-        Decrement ->
-            ( model - 1, Cmd.none )
+        DecrementGain ->
+            let
+                newModel =
+                    { model | gainValue = model.gainValue - 0.001 }
+            in
+                ( newModel, audio newModel )
+
+        IncrementFrequency ->
+            let
+                newModel =
+                    { model | frequencyValue = model.frequencyValue + 100 }
+            in
+                ( newModel, audio newModel )
+
+        DecrementFrequency ->
+            let
+                newModel =
+                    { model | frequencyValue = model.frequencyValue - 100 }
+            in
+                ( newModel, audio newModel )
+
+        UpdateDimensions { width, height } ->
+            let
+                newModel =
+                    { model | windowWidth = width, windowHeight = height }
+            in
+                ( newModel, audio newModel )
+
+        UpdateMouse ( x, y ) ->
+            let
+                -- gain is the percentage you are across the screen, from left to right, mapped from 0 to 0.03
+                newGain =
+                    ((toFloat x) / (toFloat model.windowWidth)) * 0.03
+
+                -- frequency is the percentage you are vertically down the screen, mapped from 0 to 6000
+                newFrequency =
+                    ((toFloat y) / (toFloat model.windowHeight)) * 6000.0
+
+                newModel =
+                    { model | frequencyValue = newFrequency, gainValue = newGain }
+            in
+                ( newModel, audio newModel )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
+port audio : Model -> Cmd msg
